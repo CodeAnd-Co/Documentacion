450c094394f8bbe2c993156bde9f93cf01e6cbe0
Angeltrek <angeltrek007@gmail.com>
2025-03-01 13:53:16 -0600

    arreglar links rotos - Se eliminó la carpeta de blog - Se eliminaron los links hacia /blog desde .config.js - Se eliminaron links hacia /intro en index.js

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -109,56 +109,10 @@ const config = {
             label: "Guías",
             position: "left",
           },
-          {
-            href: "https://github.com/facebook/docusaurus",
-            label: "GitHub",
-            position: "right",
-          },
         ],
       },
       footer: {
         style: "dark",
-        links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/intro",
-              },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "X",
-                href: "https://x.com/docusaurus",
-              },
-            ],
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
-                label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
-              },
-            ],
-          },
-        ],
         copyright: `Copyright © ${new Date().getFullYear()} Code&Co. Built with Docusaurus.`,
       },
       prism: {
